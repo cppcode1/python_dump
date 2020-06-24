eacf19b9cfdf4f410e4face9a3687388ded22e89
Oleksandr Chastukhin <sawer_5@mail.ru>
2020-06-24 12:00:09 +0200

    static and usual method of a class

--- python_basics.py
+++ python_basics.py
@@ -1,10 +1,25 @@
-# class fields
+# class fields, static method 
 class car:
-    min_speed = None # a static field (common)
+    """
+    this is a description of the class car
+    you can show it with help(car)
+    """
+    min_speed = None # a static field (common for all instances of the car class)
     def __init__(self, max_speed): # the constructor
         min_speed = 0 # the local variable in the constructor
         car.min_speed = min_speed # the static variable
         self.max_speed = max_speed # the usual field of the instance of the class
+
+    @staticmethod
+    def compare(one, two):
+        # one-line if
+        return "yes" if one.max_speed > two.max_speed else "no"
+
+    def name(self):
+        """
+        it will return a name of a car
+        """
+        pass
 
 my_car = car(100)
 his_car = car(200)
@@ -12,6 +27,7 @@ his_car = car(200)
 print(my_car.max_speed) # 100
 print(his_car.max_speed) # 200
 print(car.min_speed) # 0
+print(f"my car is faster: {car.compare(my_car, his_car)}")
 
 exit(0)
 
