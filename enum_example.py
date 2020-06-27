dc43b1cfad80e952372c7b518963fa309b51d099
Oleksandr Chastukhin <sawer_5@mail.ru>
2020-06-27 20:30:07 +0200

    enums in python

---
+++ enum_example.py
@@ -0,0 +1,11 @@
+"""
+this is small exapmle demonstrates how to use enums in python
+https://docs.python.org/3/library/enum.html
+"""
+from enum import Enum
+
+class operation_code(Enum):
+    authorize = 1
+    log_out = 2
+
+print(operation_code.authorize == 1)
