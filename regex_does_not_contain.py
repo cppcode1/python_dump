4311cad444d48ae90793574706f954d5344bb366
Oleksandr Chastukhin <itspeciatistpub@gmail.com>
2020-11-22 11:14:03 +0100

    regex negative look ahead

---
+++ regex_does_not_contain.py
@@ -0,0 +1,32 @@
+"""
+this example uses a negative look ahead (?!.*typedef) to
+find a string that doesn't contain another string.
+"""
+import re
+import os
+
+os.system("clear")
+
+code = """
+typedef struct {
+    code1;
+} name1;
+typedef struct {
+    code2;
+} name2;
+"""
+
+#print(code)
+match = re.search("typedef struct(?!.*typedef)(.*)name2;", code, re.DOTALL)
+if match:
+    print(match.group(0))
+else:
+    print("none")
+
+"""
+the result is
+
+typedef struct {
+    code2;
+} name2;
+"""
