16b4adfe15d5ef463d67c5fa574ddf23c33aedfc
Oleksandr Chastukhin <oleksandr.chastukhin@nokia.com>
2020-11-24 14:27:41 +0100

    better import

--- pobyt.py
+++ pobyt.py
@@ -1,15 +1,15 @@
 #!/bin/python
-import datetime
+from datetime import date
 import math
 
 # arrive and depart visa dates
 visas = []
-visas.append((datetime.date(2015, 11, 2), datetime.date(2016, 4, 24)))
-visas.append((datetime.date(2016, 7, 1), datetime.date(2016, 9, 27)))
-visas.append((datetime.date(2016, 11, 24), datetime.date(2017, 3, 5)))
-visas.append((datetime.date(2017, 11, 19), datetime.date(2018, 11, 16)))
-visas.append((datetime.date(2018, 11, 17), datetime.date(2019, 12, 31)))
-visas.append((datetime.date(2020, 1, 1), datetime.date.today()))
+visas.append((date(2015, 11, 2), date(2016, 4, 24)))
+visas.append((date(2016, 7, 1), date(2016, 9, 27)))
+visas.append((date(2016, 11, 24), date(2017, 3, 5)))
+visas.append((date(2017, 11, 19), date(2018, 11, 16)))
+visas.append((date(2018, 11, 17), date(2019, 12, 31)))
+visas.append((date(2020, 1, 1), date.today()))
 
 counter = 1
 for visa in visas:
