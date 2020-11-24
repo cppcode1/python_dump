be4407eceb4ff1659dbcea4d0391af207bbfc3fd
Oleksandr Chastukhin <oleksandr.chastukhin@nokia.com>
2020-11-24 14:35:50 +0100

    the shortcuts were added

--- pobyt.py
+++ pobyt.py
@@ -1,15 +1,16 @@
 #!/bin/python
-from datetime import date
+from datetime import date as d
 import math
 
 # arrive and depart visa dates
 visas = []
-visas.append((date(2015, 11, 2), date(2016, 4, 24)))
-visas.append((date(2016, 7, 1), date(2016, 9, 27)))
-visas.append((date(2016, 11, 24), date(2017, 3, 5)))
-visas.append((date(2017, 11, 19), date(2018, 11, 16)))
-visas.append((date(2018, 11, 17), date(2019, 12, 31)))
-visas.append((date(2020, 1, 1), date.today()))
+add = visas.append
+add((d(2015, 11, 2), d(2016, 4, 24)))
+add((d(2016, 7, 1), d(2016, 9, 27)))
+add((d(2016, 11, 24), d(2017, 3, 5)))
+add((d(2017, 11, 19), d(2018, 11, 16)))
+add((d(2018, 11, 17), d(2019, 12, 31)))
+add((d(2020, 1, 1), d.today()))
 
 counter = 1
 for visa in visas:
